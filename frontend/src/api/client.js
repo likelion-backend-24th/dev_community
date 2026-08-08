075a7eb3a2af03b0b228497611e95e47850f5438
김재혁 <kjhkmn80@gmail.com>
2026-08-08 20:41:23 +0900

    feat: axios 요청 인터셉터 추가

---
+++ frontend/src/api/client.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const client = axios.create({
+  baseURL: 'http://localhost:8080',
+})
+
+client.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem('accessToken')
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`
+  }
+  return config
+})
+
+export default client
