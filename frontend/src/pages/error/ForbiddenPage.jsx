2d331a9726c07e4d82e69a819bdded3685f8aa95
김재혁 <kjhkmn80@gmail.com>
2026-08-10 11:07:41 +0900

    feat: 401 페이지 추가 / 403 페이지 명세에 맞게 수정

--- frontend/src/pages/error/ForbiddenPage.jsx
+++ frontend/src/pages/error/ForbiddenPage.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
+
 function ForbiddenPage() {
   return (
     <div>
       <h1>403 - 접근 권한이 없습니다</h1>
+      <p>요청하신 작업을 수행할 권한이 없습니다.</p>
+      <Link to="/">목록으로</Link>
     </div>
   )
 }
---
+++ frontend/src/pages/error/UnauthorizedPage.jsx
@@ -0,0 +1,17 @@
+import { Link, useLocation } from 'react-router-dom'
+
+function UnauthorizedPage() {
+  const location = useLocation()
+  const message = location.state?.message ?? '로그인이 필요합니다.'
+
+  return (
+    <div>
+      <h1>401 - 로그인이 필요합니다</h1>
+      <p>{message}</p>
+      <Link to="/login">로그인하기</Link>
+      <Link to="/">목록으로</Link>
+    </div>
+  )
+}
+
+export default UnauthorizedPage
